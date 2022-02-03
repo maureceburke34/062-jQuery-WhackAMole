1eedc63724227a0479994181ab20627e0047e049
maureceburke34 <97970406+maureceburke34@users.noreply.github.com>
2022-02-03 10:29:20 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+$("button.whack1").click(function(){
+    $(".mole1").hide();
+});
+    
+$("button.whack2").click(function(){
+    $(".mole-two").hide();
+});
+    
+$("button.whack3").click(function(){
+    $(".third-mole").hide();
+});
+    
+$("button.reset").click(function(){
+    $("img").show();
+});
